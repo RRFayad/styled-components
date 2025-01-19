b52cc5cfdb7ec06a55bb49ea8fa107ba2d6f2ea1
RRFayad <renan.r.fayad@gmail.com>
2025-01-19 18:40:55 -0300

    feat: Styled Button - Dynamic Styles

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,23 +1,39 @@
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface ButtonProps {
   children: React.ReactElement | string;
   primary?: boolean;
+  bgColor?: string;
 }
 
-const StyledButton = styled.button<{ $primary?: boolean }>`
+interface StyledButtonProps {
+  $primary?: boolean;
+  $bgColor?: string;
+}
+
+const StyledButton = styled.button<StyledButtonProps>`
   padding: 1rem 1.5rem;
   font-size: 1.8rem;
-  color: ${(props) => (props.$primary ? "red" : "#fff")};
+  color: #fff;
   outline: none;
   border: none;
   background-color: #333;
   margin-bottom: 1rem;
+  ${({ $primary, $bgColor }) =>
+    $primary &&
+    css`
+      color: red;
+      background-color: ${$bgColor};
+    `}
 `;
 
-function Button({ primary, children }: ButtonProps) {
-  return <StyledButton $primary={primary}>{children}</StyledButton>;
+function Button({ primary, bgColor, children }: ButtonProps) {
+  return (
+    <StyledButton $primary={primary} $bgColor={bgColor}>
+      {children}
+    </StyledButton>
+  );
 }
 
 export default Button;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,9 @@ const MainWrapper = styled.section`
 export default function Home() {
   return (
     <MainWrapper>
-      <Button primary>Primary Button</Button>
+      <Button primary bgColor="violet">
+        Primary Button
+      </Button>
       <Button>Regular Button</Button>
     </MainWrapper>
   );
